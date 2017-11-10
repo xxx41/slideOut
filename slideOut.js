2c84d15d9799601bb608c7d9bb2da1423dbd8429
Ale Santana <alexxx41@gmail.com>
2017-11-10 16:53:49 +0000

    slideOut first commit

---
+++ slideOut.js
@@ -0,0 +1,55 @@
+//Full slide library
+
+//Slides out elements from viewport
+
+(function(window,document) {
+
+    var slideOut = function(window) {
+
+        var lib = {
+
+            //Slide+movement
+            //Check if elementID is a string, then assigns DOM element to a var
+            //and apply and remove styles on that var
+
+            slideRight: function(elementID, nextItem) {
+                if (typeof elementID != 'string') return;
+                var element = document.getElementById(elementID);
+                element.style.transition = 'left 2s';
+                element.style.left = '100vw';
+                var transition = setTimeout(function() {element.style.transition = '';},1000);
+            },
+
+            slideLeft: function(elementID, nextItem) {
+                if (typeof elementID != 'string') return;
+                var element = document.getElementById(elementID);
+                element.style.transition = 'left 2s';
+                element.style.left = '-100vw';
+                var transition = setTimeout(function() {element.style.transition = '';},1000);
+            },
+
+            slideTop: function(elementID, nextItem) {
+                if (typeof elementID != 'string') return;
+                var element = document.getElementById(elementID);
+                element.style.transition = 'top 2s';
+                element.style.top = '-100vw';
+                var transition = setTimeout(function() {element.style.transition = '';},1000);
+            },
+
+            slideBot: function(elementID, nextItem) {
+                if (typeof elementID != 'string') return;
+                var element = document.getElementById(elementID);
+                element.style.transition = 'top 2s';
+                element.style.top = '100vw';
+                var transition = setTimeout(function() {element.style.transition = '';},1000);
+            }
+        };
+
+        return lib;
+    }
+
+    if (typeof window.slideOut === "undefined") {
+		window.slideOut = slideOut();
+	}
+
+})(window,document);
